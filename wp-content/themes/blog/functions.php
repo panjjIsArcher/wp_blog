6e98457cd028cbf27c6b5273a1a47dfed9e85e40
Pan Jiajie <panjiajie@Pans-MacBook-Air.local>
2023-12-22 20:54:50 +0800

    land

--- wp-content/themes/blog/functions.php
+++ wp-content/themes/blog/functions.php
@@ -1,7 +1,8 @@
 <?php 
 
     function add_stylesheet_to_head() {
-        wp_enqueue_style('blog', get_stylesheet_uri(  ));
+        wp_enqueue_style('blog', get_stylesheet_uri());
+        wp_enqueue_style( 'land',get_template_directory_uri(  ).'/assets/css/land.css');
     };
 
   
